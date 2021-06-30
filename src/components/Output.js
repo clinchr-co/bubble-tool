7fe36668919fcdbd92212ac8a64416c215a3c862
Chris Lavery <clavery63@gmail.com>
2021-06-30 12:31:49 -0400

    allow square-based positioning

--- src/components/Output.js
+++ src/components/Output.js
@@ -4,6 +4,15 @@ const formatAttrs = ({ x, y, radius }) => `
 { x: ${x.toFixed(3)}, y: ${y.toFixed(3)}, radius: ${radius.toFixed(3)} }
 `.trim();
 
+const formatAttrsSquare = ({ x, y, radius }, i) => {
+  const leftPad = 0.5 - (8 / 9);
+  const squareX = leftPad + (16 / 9) * x;
+  const height = radius * 2;
+  return `
+${i}: Object.freeze({ x: ${squareX.toFixed(3)}, y: ${y.toFixed(3)}, height: ${height.toFixed(3)} })
+`.trim();
+};
+
 const Output = ({ bubbles, selectedIndex }) => {
   const outputRef = useRef(null);
   const [copied, setCopied] = useState(false);
